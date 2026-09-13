9d4a61fd001e8800e4997000a1a2486767678971
Research Dhamala <166215085+claygro@users.noreply.github.com>
2026-09-13 08:58:38 +0545

    Add health check route to the application

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,9 @@ app.use("/uploads", express.static("public/uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/job", router);
+app.get("/healthy", (req, res)=>{
+  res.json("Healthy routes");
+})
 app.listen(process.env.PORT, async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING);
